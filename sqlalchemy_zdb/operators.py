a8cfb4765e88e963b9c9cc9af53e913568f19efb
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-04-23 05:47:32 +0200

    Removed implicit regexp use through the LIKE operator in favor explicitly passing either regex objects or plain strings

--- sqlalchemy_zdb/operators.py
+++ sqlalchemy_zdb/operators.py
@@ -1,11 +1,11 @@
+import re
 import operator
 
 from sqlalchemy.sql.operators import match_op, like_op, between_op
 
 
 def zdb_between_op(left, right, *args, **kwargs):
-    """
-    (incorrectly) implements the BETWEEN operator for ZomboDB.
+    r"""(incorrectly) implement the BETWEEN operator.
 
     The ZomboDB documentation states:
 
@@ -14,11 +14,18 @@ def zdb_between_op(left, right, *args, **kwargs):
 
         https://github.com/zombodb/zombodb/blob/master/SYNTAX.md#operators
 
-    Could not get it to work. Instead made it return:
+    Could not get it to work. Instead made it return::
 
-        column >= :value_1 and column <= :value_2
+        column >= 5 and column <= 15
+
+    E.g.::
+
+        stmt = select([sometable]).\
+            where(sometable.c.column.between(5, 15))
+
+    Only works on integers.
+    @TODO: implement /to/
     """
-    # @TODO: implement /to/
     sql = ""
     for i, clause in enumerate(right.clauses):
         _oper = [COMPARE_OPERATORS[operator.ge],
@@ -27,6 +34,38 @@ def zdb_between_op(left, right, *args, **kwargs):
     return sql
 
 
+def zdb_like_op(left, right, c, compiler, tables, format_args):
+    r"""Implement the ``like`` operator.
+
+    In a normal context, produces the expression::
+
+        column:"foo"
+
+    E.g.::
+
+        stmt = select([sometable]).\
+            where(sometable.c.column.like("foo"))
+
+
+    In a regex context, produces the expression::
+
+        column:~"foo[a-z]"
+
+    E.g.::
+
+        stmt = select([sometable]).\
+            where(sometable.c.column.like(re.compile("foo[a-z]")))
+    """
+    from sqlalchemy_zdb.compiler import compile_clause
+
+    if isinstance(right.value, re._pattern_type):
+        _oper = ":~"
+        right.value = right.value.pattern
+    else:
+        _oper = ":"
+
+    return "%s%s\"%s\"" % (left.name, _oper, compile_clause(right, compiler, tables, format_args))
+
 COMPARE_OPERATORS = {
     operator.gt: " > ",
     operator.lt: " < ",
@@ -34,7 +73,7 @@ COMPARE_OPERATORS = {
     operator.le: " <= ",
     operator.ne: " != ",
     match_op: ":@",
-    like_op: ":~",
+    like_op: zdb_like_op,
     operator.eq: ":",
     between_op: zdb_between_op
 }
